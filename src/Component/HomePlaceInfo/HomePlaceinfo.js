716d8cb3db7cc24607f273086e85a589fca3bced
mahmudul474 <mdmahmudulla474@gmail.com>
2023-05-07 16:14:46 +0600

    btn fix

--- src/Component/HomePlaceInfo/HomePlaceinfo.js
+++ src/Component/HomePlaceInfo/HomePlaceinfo.js
@@ -8,25 +8,21 @@ import { Link, useNavigate } from "react-router-dom";
 import DataContext from "../../Context/BookingDataContex";
 import { Autocomplete, LoadScript } from "@react-google-maps/api";
 import { API_KEY, places } from "../../config";
+import "tailwindcss/tailwind.css";
 
 function HomePlaceinfo() {
+  const { data, setData } = useContext(DataContext);
 
-const {data,setData}=useContext(DataContext)
-
-const navigate=useNavigate()
-
-
+  const navigate = useNavigate();
 
   //start locations
   const [startLocationValue, setStartLocationValue] = useState("");
   const [showIcon, showStartlocationIcon] = useState(false);
 
-  
   //end locations
 
   const [endLocationValue, setEndLocationValue] = useState("");
   const [showEndicon, showEndtlocationIcon] = useState(false);
- 
 
   const handleEndLocationChange = (event) => {
     setEndLocationValue(event.target.value);
@@ -46,8 +42,7 @@ const navigate=useNavigate()
 
   const handleReturndateOpen = () => {
     setShowReturndate(true);
-    handlesaveBookingData() 
-
+    handlesaveBookingData();
   };
 
   //singeleTrip
@@ -57,8 +52,8 @@ const navigate=useNavigate()
     setReturnDate(null);
     setShowReturndate(false);
     setSingelValue("Single");
-  
-    handlesaveBookingData ()
+
+    handlesaveBookingData();
   };
 
   //wait and return
@@ -68,70 +63,53 @@ const navigate=useNavigate()
     setReturnDate(null);
     setShowReturndate(false);
     setWaitandReturnValue("wait and return");
-    handlesaveBookingData()
+    handlesaveBookingData();
   };
 
+  const [selectedBtn, setSelectedBtn] = useState("single");
 
-   
+  const handleClick = (btn) => {
+    setSelectedBtn(btn);
+  };
 
-//save data 
+  //save data
   const handlesaveBookingData = (e) => {
-
-     
-
-
     const data = {
       origin: startLocationValue ? startLocationValue : "",
       destination: endLocationValue ? endLocationValue : "",
       date: startDate ? startDate.toLocaleString() : "",
       returnDate: returnDate ? returnDate.toLocaleString() : singelvalue,
-     
       waitandReturn: waitandreturnValue ? waitandreturnValue : singelvalue,
-      
       single: singelvalue && singelvalue,
       bydefoultTrip: "single",
     };
-   
 
-    setData(data)
-    
-    
+    setData(data);
   };
 
-
-
-  console.log(data,"dddddddd")
-   
-
-  
-
   return (
-  <LoadScript
-  googleMapsApiKey={API_KEY}
-  libraries={places}
-  >
-  
-  <div className=" ">
-      <div className="my-9  ">
-        <div className=" " onClick={handlesaveBookingData} >
-          <div className=" px-6 m-auto">
-            {/* first location */}
-            <div className="relative my-5">
-          
-            <Autocomplete>
-              <input
-                required
-                type="text"
-                defaultValue={data&&  data.origin}
-                onBlur={(e)=>{
-                  setStartLocationValue(e.target.value)
-                  handlesaveBookingData()
-                }}
-                className="border border-gray-400 rounded-sm   px-16 py-5  text-md font-bold   w-full"
-                placeholder="PICKUP ADDRESS"
-              /></Autocomplete>
+    <LoadScript googleMapsApiKey={API_KEY} libraries={places}>
+      <div className=" ">
+        <div className="my-9  ">
+          <div className=" " onClick={handlesaveBookingData}>
+            <div className=" px-6 m-auto">
+              {/* first location */}
+              <div className="relative my-5">
+                <Autocomplete>
+                  <input
+                    required
+                    type="text"
+                    defaultValue={data && data.origin}
+                    onBlur={(e) => {
+                      setStartLocationValue(e.target.value);
+                      handlesaveBookingData();
+                    }}
+                    className="border border-gray-400 rounded-sm   px-16 py-5  text-md font-bold   w-full"
+                    placeholder="PICKUP ADDRESS"
+                  />
+                </Autocomplete>
 
-              {/* {startLocationValue && (
+                {/* {startLocationValue && (
                 <button
                   className="absolute top-1/2 right-9 transform -translate-y-1/2"
                   onClick={handleClearStartLocation}
@@ -142,34 +120,33 @@ const navigate=useNavigate()
                 </button>
               )} */}
 
-              <div className="absolute inset-y-0 border border-gray-600  left-0 flex items-center px-2">
-                <span className="w-10 h-10 m-auto  ">
-                  <img src="https://i.ibb.co/FhXTXP9/location.png" />
-                </span>
+                <div className="absolute inset-y-0 border border-gray-600  left-0 flex items-center px-2">
+                  <span className="w-10 h-10 m-auto  ">
+                    <img src="https://i.ibb.co/FhXTXP9/location.png" />
+                  </span>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+                  <span>
+                    <AiOutlineDown />
+                  </span>
+                </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-                <span>
-                  <AiOutlineDown />
-                </span>
-              </div>
-            </div>
 
-            {/* seconde location */}
-            <div className="relative my-5">
-              <Autocomplete>
-              <input
-                required
-                type="text"
-               
-                onBlur={(e)=>{
-                  setEndLocationValue(e.target.value)
-                  handlesaveBookingData()
-                }}
-                className="border border-gray-400 rounded-sm   px-16 py-5  text-md font-bold   w-full"
-                placeholder="DESTINATION ADDRESS"
-              />
-               </Autocomplete>
-              {/* {endLocationValue && (
+              {/* seconde location */}
+              <div className="relative my-5">
+                <Autocomplete>
+                  <input
+                    required
+                    type="text"
+                    onBlur={(e) => {
+                      setEndLocationValue(e.target.value);
+                      handlesaveBookingData();
+                    }}
+                    className="border border-gray-400 rounded-sm   px-16 py-5  text-md font-bold   w-full"
+                    placeholder="DESTINATION ADDRESS"
+                  />
+                </Autocomplete>
+                {/* {endLocationValue && (
                 <button
                   className="absolute top-1/2 right-9 transform -translate-y-1/2"
                   onClick={handleClearEndLocation}
@@ -180,87 +157,94 @@ const navigate=useNavigate()
                 </button>
               )} */}
 
-              <div className="absolute inset-y-0 border border-gray-600  left-0 flex items-center px-2">
-                <span className="w-10 h-10 m-auto  ">
-                  <img src="https://i.ibb.co/pyf6D9B/pin.png" />
-                </span>
+                <div className="absolute inset-y-0 border border-gray-600  left-0 flex items-center px-2">
+                  <span className="w-10 h-10 m-auto  ">
+                    <img src="https://i.ibb.co/pyf6D9B/pin.png" />
+                  </span>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+                  <span>
+                    <AiOutlineDown />
+                  </span>
+                </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-                <span>
-                  <AiOutlineDown />
-                </span>
+            </div>
+
+            {/* start date */}
+            <div>
+              <DateTime
+                selectedDate={startDate}
+                setSelectedDate={setStartDate}
+              ></DateTime>
+            </div>
+
+            {/* return*/}
+
+            {showreturndate && (
+              <div className="my-5">
+                {" "}
+                <ReturnDate
+                  selectedDate={returnDate}
+                  setSelectedDate={setReturnDate}
+                ></ReturnDate>{" "}
+              </div>
+            )}
+
+            <div className="my-5 mx-6">
+              {/* btn grupe */}
+              <div class="inline-flex rounded-md shadow-sm" role="group">
+                <button
+                  className={`bg-${
+                    selectedBtn === "single" ? "red" : "gray"
+                  }-500 text-${
+                    selectedBtn === "single" ? "white" : "black"
+                  } px-4 py-2 rounded-md mr-4`}
+                  onClick={() => {
+                    handleClick("single");
+                    handlesingleTrip();
+                  }}
+                >
+                  SINGLE
+                </button>
+                <button
+                  className={`bg-${
+                    selectedBtn === "return" ? "red" : "gray"
+                  }-500 text-${
+                    selectedBtn === "return" ? "white" : "black"
+                  } px-4 py-2 rounded-md mr-4`}
+                  onClick={() => {
+                    handleClick("return");
+                    handleReturndateOpen();
+                  }}
+                >
+                  RETURN
+                </button>
+                <button
+                  className={`bg-${
+                    selectedBtn === "wait" ? "red" : "gray"
+                  }-500 text-${
+                    selectedBtn === "wait" ? "white" : "black"
+                  } px-4 py-2 rounded-md mr-4`}
+                  onClick={() => {
+                    handleWaitAndReturn();
+                    handleClick("wait");
+                  }}
+                >
+                  WAIT & RETURN
+                </button>
               </div>
             </div>
-          </div>
-
-          {/* start date */}
-          <div>
-            <DateTime
-              selectedDate={startDate}
-              setSelectedDate={setStartDate}
-            ></DateTime>
-          </div>
-
-          {/* return*/}
-
-          {showreturndate && (
-            <div className="my-5">
-              {" "}
-              <ReturnDate
-                selectedDate={returnDate}
-                setSelectedDate={setReturnDate}
-              ></ReturnDate>{" "}
+            <div className="flex justify-end items-end mr-5">
+              <Link to="/booking">
+                <button className="btn bg-green-950 w-36 hover:bg-green-950  text-white font-bold capitalize">
+                  NEXT
+                </button>
+              </Link>
             </div>
-          )}
-
-
-
- 
-
-          <div className="my-5 mx-6">
-            {/* btn grupe */}
-            <div class="inline-flex rounded-md shadow-sm" role="group">
-              <button
-                onClick={handlesingleTrip}
-                type="button"
-                className="bg-transparent  border focus:border-none border-gray-400 font-bold capitalize p-3  mr-3 focus:bg-green-950 focus:text-white "
-              >
-                SINGLE
-              </button>
-              <button
-                onClick={handleReturndateOpen}
-                type="button"
-                className="bg-transparent  border focus:border-none border-gray-400 font-bold capitalize p-3  mr-3 focus:bg-green-950 focus:text-white "
-              >
-                RETURN
-              </button>
-              <button
-                onClick={handleWaitAndReturn}
-                type="button"
-                className="bg-transparent  border focus:border-none border-gray-400 font-bold capitalize p-3  mr-3 focus:bg-green-950 focus:text-white "
-              >
-                WAIT & RETURN
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-end items-end mr-5">
-          
-          <Link to="/booking">
-            <button
-              
-              className="btn bg-green-950 w-36 hover:bg-green-950  text-white font-bold capitalize"
-            >
-              NEXT
-            </button></Link>
           </div>
         </div>
       </div>
-    </div>
-  
-  
-  </LoadScript>
+    </LoadScript>
   );
-
-
 }
 export default HomePlaceinfo;
---
+++ src/Shared/test/ButtonGroup.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function ButtonGroup() {
+  
+
+  return (
+    <h2>d</h2>
+  );
+}
+
+export default ButtonGroup;
